5e7dc7d5503749c5c0fc7414e00858c7e8e386cc
Moamen Khalaf <moamenkhalaf.work@gmail.com>
2024-12-07 18:13:35 +0200

    docs: update README with simulation table examples; add images for normal and probability-based simulations
    feat: integrate ToolTip component into ChartDrawer for enhanced user interaction

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ npm run dev
 ```
 npm run deploy
 ```
+
+normal simulation accepted excel table be like :
+![alt text](image.png)
+
+---
+
+probability based simulation accepted excel table be like :
+![alt text](image-1.png)
--- client/src/components/simulation/ChartDrawer.tsx
+++ client/src/components/simulation/ChartDrawer.tsx
@@ -1,13 +1,16 @@
 import { ChartLine } from "lucide-react";
 
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import { ToolTip } from "../ToolTip";
 import { Chart } from "./Chart";
 
 export function DrawerDemo() {
   return (
     <Drawer>
       <DrawerTrigger asChild>
-        <ChartLine className="w-14 hover:text-blue-500 hover:dark:text-white cursor-pointer" />
+        <ToolTip message="Chart">
+          <ChartLine className="w-14 hover:text-blue-500 hover:dark:text-white cursor-pointer" />
+        </ToolTip>
       </DrawerTrigger>
       <DrawerContent>
         <div className="w-full md:w-[50%] md:h-1/2 mx-auto my-2">
