c0c666fdac29f83a954ce82f70e6b2b0406a5622
Moamen Khalaf <moamenkhalaf.work@gmail.com>
2024-12-15 02:23:34 +0200

    feat: refactor table input handling by creating TableInputBar component; improve state management for selected cells

---
+++ client/src/components/simulation/TopBar/TableInputBar.tsx
@@ -0,0 +1,54 @@
+import { useTable } from "@/store/table";
+import { useEffect, useState } from "react";
+import { Check } from "lucide-react";
+export default function TableInputBar() {
+  const selectedCell = useTable((state) => state.selectedCell);
+  const [cell, setCell] = useState(selectedCell);
+  useEffect(() => {
+    const unsubscribe = useTable.subscribe(
+      (state) => state.selectedCell,
+      (current) => {
+        setCell(current);
+      }
+    );
+    return () => unsubscribe();
+  }, []);
+  const handleChange = (newValue: string) => {
+    if (newValue.startsWith("=")) {
+      setCell({ ...cell, f: newValue, v: "" });
+    } else {
+      setCell({ ...cell, v: newValue, f: "" });
+    }
+  };
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="A1"
+        value={cell.pos || ""}
+        className="bg-transparent focus:outline-none w-14 text-center px-auto py-2 border-r border-gray-400 font-bold"
+      />
+      <input
+        type="text"
+        value={cell.f || cell.v || ""}
+        onChange={(e) => handleChange(e.target.value)}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            useTable.getState().setCell(cell);
+          }
+        }}
+        className="outline-none bg-transparent focus:outline-none w-full px-4 py-2"
+        placeholder="=SUM(A1:A2) etc."
+      />
+      {JSON.stringify(cell) !== JSON.stringify(selectedCell) &&
+        (selectedCell.pos.length ? (
+          <Check
+            className="w-14 hover:text-green-500 hover:dark:text-white cursor-pointer"
+            onClick={() => {
+              useTable.getState().setCell(cell);
+            }}
+          />
+        ) : null)}
+    </>
+  );
+}
--- client/src/components/simulation/TopBar/TableTopBar.tsx
+++ client/src/components/simulation/TopBar/TableTopBar.tsx
@@ -1,51 +1,10 @@
-import { useTable } from "@/store/table";
+import TableInputBar from "./TableInputBar";
 import TableOptions from "./TableOptions";
-import { useEffect, useState } from "react";
-import { Check } from "lucide-react";
 
 export function TableTopBar() {
-  const selectedCell = useTable((state) => state.selectedCell);
-  const [cell, setCell] = useState(selectedCell);
-  useEffect(() => {
-    const unsubscribe = useTable.subscribe(
-      (state) => state.selectedCell,
-      (current) => {
-        setCell(current);
-      }
-    );
-    return () => unsubscribe();
-  }, []);
-
   return (
     <div className="flex items-center bg-secondary w-full border border-foreground text-primary">
-      <input
-        type="text"
-        placeholder="A1"
-        value={cell.pos || ""}
-        className="bg-transparent focus:outline-none w-14 text-center px-auto py-2 border-r border-gray-400 font-bold"
-      />
-      <input
-        type="text"
-        value={cell.f || cell.v || ""}
-        onChange={(event) => {
-          if (event.target.value.startsWith("=")) {
-            setCell({ ...cell, f: event.target.value });
-          } else {
-            setCell({ ...cell, v: event.target.value });
-          }
-        }}
-        className="outline-none bg-transparent focus:outline-none w-full px-4 py-2"
-        placeholder="=SUM(A1:A2) etc."
-      />
-      {JSON.stringify(cell) !== JSON.stringify(selectedCell) &&
-        (selectedCell.pos.length ? (
-          <Check
-            className="w-14 hover:text-green-500 hover:dark:text-white cursor-pointer"
-            onClick={() => {
-              useTable.getState().setCell(cell);
-            }}
-          />
-        ) : null)}
+      <TableInputBar />
       <TableOptions />
     </div>
   );
